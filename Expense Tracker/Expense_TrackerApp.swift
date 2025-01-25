86ef22a2cd7180b5d41624ecb4beb3f635dff545
dstark12 <143757465+dstark12@users.noreply.github.com>
2025-01-25 09:22:24 -0500

    Initial Commit

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Dustin ￼Stark on 1/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Expense TrackerTests/Expense_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerTests.swift
+//  Expense TrackerTests
+//
+//  Created by Dustin ￼Stark on 1/25/25.
+//
+
+import Testing
+@testable import Expense_Tracker
+
+struct Expense_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
